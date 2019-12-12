dc28b9dd2dce956cb3df06f5771b593fe86a974b
Wim van Berlo <wberlo@users.noreply.github.com>
2019-12-12 18:36:07 +0100

    Add files via upload

---
+++ mqtt/boot_git.py
@@ -0,0 +1,42 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+ssid = 'your ssid'
+password = 'your passphrase'
+mqtt_server = 'your broker ip address'
+
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'notification'
+topic_pub = b'SQM'
+topic_pub_time = b'Date'
+
+last_message = 0
+message_interval = 5
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+    print(".", end="")
+    time.sleep(1)
+
+print('Connection successful')
+print(station.ifconfig())
+
+from ntptime import settime
+settime()
+
+
+
